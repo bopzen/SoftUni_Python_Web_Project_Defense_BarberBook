4b3fedf11e387f6d81a19fcc2f47db92a3d3a13a
bopzen <boppp@abv.bg>
2023-07-03 15:32:51 +0300

    Main URLs added

--- BarberBook/urls.py
+++ BarberBook/urls.py
@@ -1,22 +1,14 @@
-"""
-URL configuration for BarberBook project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('BarberBook.common.urls')),
+    path('accounts/', include('BarberBook.accounts.urls')),
+    path('barbers/', include('BarberBook.barbers.urls')),
+    path('barbershops/', include('BarberBook.barbershops.urls')),
+    path('clients/', include('BarberBook.clients.urls')),
+    path('reservations/', include('BarberBook.reservations.urls')),
+    path('reviews/', include('BarberBook.reviews.urls'))
 ]
