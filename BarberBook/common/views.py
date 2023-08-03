00b92af2a9f519a998895e747f14ba12d7e06987
bopzen <boppp@abv.bg>
2023-08-03 23:02:48 +0300

    - Adding new features to home page

--- BarberBook/common/views.py
+++ BarberBook/common/views.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.db.models import Q
 from django.http import JsonResponse
 from django.shortcuts import render
@@ -9,9 +11,27 @@ from BarberBook.reservation.models import Reservation
 def home_page(request):
     barbershops_count = BarbershopProfile.objects.all().count()
     reservations_count = Reservation.objects.all().count()
+
+    user = request.user
+
+    client_message = None
+    if user.is_authenticated and hasattr(user, 'clientprofile'):
+        total_reservations = Reservation.objects.filter(user=user).count()
+        client_message = f"You have booked {total_reservations} appointments so far."
+
+    barbershop_message = None
+    if user.is_authenticated and hasattr(user, 'barbershopprofile'):
+        today_reservations = Reservation.objects.filter(barbershop=user.barbershopprofile, date=datetime.date.today()).count()
+        if today_reservations == 1:
+            barbershop_message = f"You have {today_reservations} reservation for today"
+        else:
+            barbershop_message = f"You have {today_reservations} reservations for today"
+
     context = {
         'barbershops_count': barbershops_count,
         'reservations_count': reservations_count,
+        'client_message': client_message,
+        'barbershop_message': barbershop_message,
     }
     return render(request, 'common/home.html', context)
 
